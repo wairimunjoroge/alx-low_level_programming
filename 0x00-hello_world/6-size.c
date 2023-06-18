afc97bb993036b9b88cc58d996c4114e528521d1
wairimunjoroge <wairimunjoroge708@gmail.com>
2023-06-18 15:18:25 +0000

    line program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,8 @@ int main(void)
 	printf("Size of char: %lu byte(s)\n", sizeof(char));
 	printf("Size of int: %lu byte(s)\n", sizeof(int));
 	printf("Size of long: %lu byte(s)\n", sizeof(long));
-	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of float: %lu byte(s)\n", sizeof(float));
-	printf("Size of double: %lu byte(s)\n", sizeof(double));
-	printf("Size of long double: %lu byte(s)\n", sizeof(long double));
 	return 0;
 }

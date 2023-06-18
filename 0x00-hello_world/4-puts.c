08cac2b49676fdf71b1ac158fb9d1e85a34f8a7b
wairimunjoroge <wairimunjoroge708@gmail.com>
2023-06-18 14:35:11 +0000

    c program to print a sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+
 int main(void) 
 {
-	puts("Programming is like building a multilingual puzzle,"); 
+	puts("\"Programming is like building a multilingual puzzle,"); 
 	return (0);
 }
 

d210edf9a7c7fae853bc530f0fdd45904e56089f
wairimunjoroge <wairimunjoroge708@gmail.com>
2023-06-19 12:48:10 -0700

    alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,13 @@
 int main(void)
 {
 	char letter;	
-	for (letter = 'a'; letter != 'e' && letter != 'q'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+	{
 		putchar(letter);
+	}
+	}
 
 	putchar('\n');
 	return (0);

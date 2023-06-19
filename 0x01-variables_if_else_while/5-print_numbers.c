cabd11d8185cbe78992c2921b2dc76be6abc6354
wairimunjoroge <wairimunjoroge708@gmail.com>
2023-06-19 13:20:55 -0700

    numbers to the base of ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int n = 10;
+	
+	if (n < 10)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}

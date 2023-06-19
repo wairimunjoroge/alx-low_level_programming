9b5a7688c1632a7078dfed4c19678044ddf224ee
wairimunjoroge <wairimunjoroge708@gmail.com>
2023-06-19 13:16:25 +0000

    else if command

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,16 +18,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf( "%d", n);
-	
+	printf("%d", n);
 	if (n > 0)
-		printf( "is positive\n");
+		printf("is positive\n");
 	else if (n == 0)
 	{
-		printf( "is zero\n");
+		printf("is zero\n");
 	} else
 	{
-		printf( "is negative\n");
+		printf("is negative\n");
 	}
 	return (0);
 }

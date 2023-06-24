0ef160fb6dbf7f0de683de3a672727e7f8b9043c
wairimunjoroge <wairimunjoroge708@gmail.com>
2023-06-24 21:28:15 +0000

    character sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_sign - prints the sign of a number
@@ -11,15 +10,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+%d\n", n);
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0\n");
+		_putchar('0');
 		return (0);
 	}
 	else
-		printf("-%d\n", -n);
+		_putchar('-');
 	return (-1);
 }

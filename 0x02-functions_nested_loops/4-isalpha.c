2c6f277b14072231d3397f0e007547d12ee3e5a8
wairimunjoroge <wairimunjoroge708@gmail.com>
2023-06-24 14:09:57 +0000

    alphabet checker

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,12 +8,8 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		if (c >= 'A' && c <= 'Z')
-		{
-			return (1);
-		}
 		return (1);
 	}
 	else

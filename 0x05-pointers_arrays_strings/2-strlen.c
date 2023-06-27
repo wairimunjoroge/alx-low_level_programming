43da6ff1288e32fc517bdbe3d335aba471e87067
wairimunjoroge <wairimunjoroge708@gmail.com>
2023-06-27 05:30:39 +0000

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns length of a string
+ * @s: parameter
+ *
+ * Retur: string length
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}

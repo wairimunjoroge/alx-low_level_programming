fd3207085a5cc5382933090173c23e910d82c9e8
wairimunjoroge <wairimunjoroge708@gmail.com>
2023-06-27 11:20:20 +0000

    printing a string using -puts function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * _puts - prints a string
+ *@str: the string to be printed
+ * Return: a string
+ */
+void _puts(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr)
+	{
+		write(1, ptr,1);
+		ptr++;
+	}
+	write(1, "\n", 1);
+}

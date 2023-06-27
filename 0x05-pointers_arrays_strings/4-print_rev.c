b3d78f2403d6117a66a4d18523f721eb8874b753
wairimunjoroge <wairimunjoroge708@gmail.com>
2023-06-27 16:09:01 -0700

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints in reverse
+ * @s: the string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int x = strlen(s) - 1;
+	int y;
+	char z;
+
+	for (y = x; y >= 0; y--)
+	{
+		z = s[y];
+		_putchar(z);
+	}
+
+
+	_putchar('\n');
+}

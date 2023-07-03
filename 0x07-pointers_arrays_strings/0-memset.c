24d4326bad798c62db2cb568539c602fb98de2ad
wairimunjoroge <wairimunjoroge708@gamail.com>
2023-07-03 16:04:42 -0700

    pointing to the designated memory area

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory area pointed by the constant byte b
+ * @s: pointer to the memory area to be filled
+ * @b: the constant byte to fill the memory area
+ * @n: the number of bytes to be filled
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}

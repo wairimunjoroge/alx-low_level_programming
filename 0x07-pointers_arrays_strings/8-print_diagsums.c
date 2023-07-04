d5747fae3f7d9aa49e9dde590512d4f44e70ccc5
wairimunjoroge <wairimunjoroge708@gamail.com>
2023-07-04 15:21:34 -0700

    sum of diagonal matrix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,23 +8,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
 	int sum1 = 0;  /* Sum of main diagonal */
 	int sum2 = 0;  /* Sum of secondary diagonal */
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-				/* Accessing element on the main diagonal */
-				sum1 += *(a + (i * size) + j);
-			if (i + j == size - 1)
-				/* Accessing element on the secondary diagonal */
-				sum2 += *(a + (i * size) + j);
-		}
+		sum1 += a[(size + 1) * i];
+		sum2 += a[(size - 1) * (i + 1)];
 	}
-
-	printf("Sum of main diagonal: %d\n", sum1);
-	printf("Sum of secondary diagonal: %d\n", sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
